95bb7040e24b70078c5a048466b3992939a79e97
Nhoj <nhojplummer@yahoo.com>
2022-05-04 18:32:02 -0400

    Fixed The issue with CSS and Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,24 @@
-const path = require("path")
+const path = require('path')
 
 const postCSSPlugins = [
-  require("postcss-simple-vars"), 
-  require("postcss-nested"), 
-  require("autoprefixer")
+  require('postcss-simple-vars'), 
+  require('postcss-nested'), 
+  require('autoprefixer')
 ]
 
 module.exports = {
-  entry: "./app/assets/scripts/App.js",
+  entry: './app/assets/scripts/App.js',
   output: {
-    filename: "bundled.js",
-    path: path.resolve(__dirname, "app")
+    filename: 'bundled.js',
+    path: path.resolve(__dirname, 'app')
   },
-  mode: "development",
+  mode: 'development',
   watch: true,
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader", { loader: "postcss-loader", options: { postcssOptions: { plugins: postCSSPlugins } } }]
+        use: ['style-loader', 'css-loader', {loader: 'postcss-loader', options: {postcssOptions: {plugins: postCSSPlugins}}}]
       }
     ]
   }
