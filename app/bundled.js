6ab2620ed4e91bbd60f9efc01ba4d88abfff0df4
Nhoj <nhojplummer@yahoo.com>
2022-05-08 23:37:46 -0400

    Webpack Timeout

--- app/bundled.js
+++ app/bundled.js
@@ -16,7 +16,7 @@
   \***********************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _styles_styles_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../styles/styles.css */ \"./app/assets/styles/styles.css\");\n\r\n\n\n//# sourceURL=webpack://travel-site/./app/assets/scripts/App.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _styles_styles_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../styles/styles.css */ \"./app/assets/styles/styles.css\");\n\r\n\r\nif (false) {}\r\n\n\n//# sourceURL=webpack://travel-site/./app/assets/scripts/App.js?");
 
 /***/ }),
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,6 @@
+const { setServers } = require('dns')
+const { appendFile } = require('fs')
+const { hostname } = require('os')
 const path = require('path')
 
 const postCSSPlugins = [
@@ -13,8 +16,17 @@ module.exports = {
     filename: 'bundled.js',
     path: path.resolve(__dirname, 'app')
   },
+  devServer: {
+    watchFiles: ('./app/**/*.html'),
+    static: {
+      directory: path.join(__dirname, 'app'),
+    },
+    hot: true,
+    host: '192.168.3.190',
+    port: 3000,
+    liveReload: false
+  },
   mode: 'development',
-  watch: true,
   module: {
     rules: [
       {
