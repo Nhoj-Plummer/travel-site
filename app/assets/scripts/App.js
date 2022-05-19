be3552c8dc16cf1b0a9a7c3f6a6eef85499d37a0
Nhoj <nhojplummer@yahoo.com>
2022-05-19 19:18:10 -0400

    Checked over to try some new JavaScript

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,3 +3,19 @@ import '../styles/styles.css'
 if (module.hot) {
     module.hot.accept()
 }
+
+/* Lesson example code below this line */
+function Person(fullName, favColor) {
+    this.name = fullName;
+    this.favoriteColor = favColor;
+    this.greet = function() {
+        console.log("Hello my name is " + this.name + " and my favorite color is " + this.favoriteColor + ".");
+    }
+}
+
+var john = new Person("John Doe", "blue");
+john.greet();
+
+
+var jane = new Person("Jane Smith", "green");
+jane.greet();
