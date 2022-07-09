27a86c1ed158442aa63f88c6d72ab5beb3b8e916
Nhoj <nhojplummer@yahoo.com>
2022-07-08 21:36:53 -0400

    Changed a few pieces of code

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -5,6 +5,15 @@ import RevealOnScroll from './modules/RevealOnScroll'
 import StickyHeader from './modules/StickyHeader'
 import ClientArea from './modules/ClientArea'
 
+// React Related Code Goes Here
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+//IMport React components that we created
+import MyAmazingComponent from './modules/MyAmazingComponent'
+
+ReactDOM.render(<MyAmazingComponent />, document.querySelector("#my-react-example"))
+
 
 new ClientArea()
 new StickyHeader()
